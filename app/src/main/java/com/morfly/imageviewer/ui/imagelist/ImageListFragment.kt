74f3712c1e9ced592748f0d53dac151f941b4c57
Pavlo Stavytskyi <morfly3000@gmail.com>
2019-12-14 15:28:22 +0200

    initial setup

---
+++ app/src/main/java/com/morfly/imageviewer/ui/imagelist/ImageListFragment.kt
@@ -0,0 +1,11 @@
+package com.morfly.imageviewer.ui.imagelist
+
+import androidx.fragment.app.Fragment
+
+class ImageListFragment: Fragment() {
+
+    companion object {
+
+        fun newInstance() = ImageListFragment()
+    }
+}
---
+++ app/src/main/java/com/morfly/imageviewer/ui/imagelist/ImageListRecyclerView.kt
@@ -0,0 +1,19 @@
+package com.morfly.imageviewer.ui.imagelist
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageListRecyclerView: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
